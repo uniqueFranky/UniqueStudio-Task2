6e7346304c7f93ee1dca58a7531a1c77ccf1e51d
uniqueFranky <y_r_b@qq.com>
2022-04-09 20:11:04 +0800

    Initial Commit

---
+++ UniqueStudio-Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniqueStudio-Task2
+//
+//  Created by 闫润邦 on 2022/4/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
